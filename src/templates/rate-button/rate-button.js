d735fd0d609f98748292bd171cac9c85dde00352
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-05-15 02:07:05 +0300

    Пересборка, подключение jquery плагина

---
+++ src/templates/rate-button/rate-button.js
@@ -0,0 +1,16 @@
+import 'jquery-bar-rating/dist/jquery.barrating.min';
+
+var a = null;
+
+$(function(){
+    a = $('.js-widget-rate-button').attr('data-rating');
+});
+
+$(function() {
+    $('.js-widget-rate-button').barrating({
+        theme: 'fontawesome-stars',
+        showSelectedRating: false,
+        showValues: false,
+        initialRating: a
+    });
+});
