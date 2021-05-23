202501f01e14b7e2a704de68fd4fdca190223c22
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-05-23 13:50:22 +0300

    Исправлены js файлы like-button

---
+++ src/templates/like-button/like-button-init.js
@@ -0,0 +1,7 @@
+import LikeButton from './like-button';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const likeButtons = document.querySelectorAll('.js-like-button');
+
+    likeButtons.forEach((item) => new LikeButton(item));
+});
--- src/templates/like-button/like-button.js
+++ src/templates/like-button/like-button.js
@@ -1,31 +1,53 @@
-let clicked = $('.like-btn').attr('data');
-let count = document.querySelector('.like-btn__count');
-let icon = document.querySelector('.material-icons');
+class LikeButton {
+  constructor(element) {
+      this.button = element;
 
-$(function(){
-    if (clicked=="true") {
-        $('.like-btn').attr('class', 'like-btn like-btn__bl-active');
-        $('.like-btn__icon').attr('class', 'like-btn__icon like-btn__icon__bl-active');
-        icon.textContent = 'favorite';
-    } else {
-        $('.like-btn').attr('class', 'like-btn');
-        $('.like-btn__icon').attr('class', 'like-btn__icon');
-        icon.textContent = 'favorite_border';
-    }
-});
+      this.findDomElement();
+      this.bindEventListener();
+      this.setStage();
+  }
+  
+  findDomElement() {
+      this.heartContainer = this.button.querySelector('.js-like-button__icon');
+      this.stageData = this.button.getAttribute('data');
+      this.countLikes = this.button.querySelector('.js-like-button__count');
+      this.heart = this.button.querySelector('.material-icons');
+  }
+  bindEventListener() {
+      this.button.addEventListener('click', this.handleButtonClick.bind(this));
+  }
+  handleButtonClick() {
+      this.button.classList.toggle('like-button_active');
+  
+      if(this.button.className.includes('like-button_active')) {
+          this.setActive();
+          this.countLikes.textContent = Number(this.countLikes.textContent) + 1;
+      }
+      else {
+          this.setUnActive();
+          if(this.countLikes.textContent >= 0) {
+          this.countLikes.textContent = Number(this.countLikes.textContent) - 1;
+          }
+      }
+  }
+  setStage() {
+      if(this.stageData === 'true') {
+          this.setActive();
+      }
+      else if(this.stageData === 'false') {
+          this.setUnActive();
+      }
+  }
+  setActive() {
+      this.button.classList.add('like-button_active');
+      this.heartContainer.classList.add('like-button__icon_active');
+      this.heart.textContent='favorite';
+  }
+  setUnActive() {
+      this.button.classList.remove('like-button_active');
+      this.heartContainer.classList.remove('like-button__icon_active');
+      this.heart.textContent='favorite_border';
+  }
+}
 
-$('.like-btn').click(function(){
-    if (clicked=="false") {
-        clicked = "true";
-        $('.like-btn').attr('class', 'like-btn like-btn__bl-active');
-        $('.like-btn__icon').attr('class', 'like-btn__icon like-btn__icon__bl-active');
-        icon.textContent = 'favorite';
-        count.textContent++;
-    } else {
-        clicked = "false";
-        $('.like-btn').attr('class', 'like-btn');
-        $('.like-btn__icon').attr('class', 'like-btn__icon');
-        icon.textContent = 'favorite_border';
-        count.textContent--;
-    }
-});
+export default LikeButton;
