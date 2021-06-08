a8bc2e55c59f244e06c6a081eb19c2512e204398
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-06-08 20:18:31 +0300

    Рефакторинг шаблона date-dropdown

--- src/templates/date-dropdown/date-dropdown.js
+++ src/templates/date-dropdown/date-dropdown.js
@@ -3,11 +3,14 @@ import Calendar from '../calendar/calendar';
 class DateDropdown {
     constructor(container) {
         this.dateDrop = container;
+
         this.dateDropArrival = this.dateDrop.querySelector(".js-date-dropdown-arrival");
         this.dateDropDepature = this.dateDrop.querySelector(".js-date-dropdown-depature");
+        this.dateDropSingle = this.dateDrop.querySelector(".js-date-dropdown-single");
+
         this.isCalOpen = this.dateDrop.getAttribute("data-calendar-isopen");
 
-        new Calendar(this.dateDrop, true, this.isCalOpen, this.dateDropArrival, this.dateDropDepature);
+        new Calendar(this.dateDrop, this.isCalOpen, this.dateDropArrival, this.dateDropDepature);
     }
     
 }
