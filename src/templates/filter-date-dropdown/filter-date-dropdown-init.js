29f66173760b6820df411dd8af2e896f3c0d9872
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-06-08 20:19:13 +0300

    Добавление шаблона filter-date-dropdown

---
+++ src/templates/filter-date-dropdown/filter-date-dropdown-init.js
@@ -0,0 +1,8 @@
+import FilterDateDropdown from "./filter-date-dropdown";
+
+$(() => {
+    let $FilterDD = $('.js-filter-date-dropdown');
+    $FilterDD.each((i, val) => {
+        new FilterDateDropdown(val);
+    });   
+});
---
+++ src/templates/filter-date-dropdown/filter-date-dropdown.js
@@ -0,0 +1,15 @@
+import Calendar from '../calendar/calendar';
+
+class FilterDateDropdown {
+    constructor(container) {
+        this.dateDrop = container;
+
+        this.dropInput = this.dateDrop.querySelector(".js-filter-date-dropdown__input");
+
+        this.isOpen = this.dateDrop.getAttribute("data-calendar-isopen");
+
+        new Calendar(this.dateDrop, this.isOpen, this.dropInput);
+    }
+    
+}
+export default FilterDateDropdown;
