1d8adf44b8851cec7578ee684aaa7ef7e41d9a5c
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-07-03 12:55:51 +0300

    Рефакторинг rate-button

--- src/templates/rate-button/rate-button.js
+++ src/templates/rate-button/rate-button.js
@@ -1,29 +1,31 @@
 import 'jquery-bar-rating/dist/jquery.barrating.min';
 
 class RateButton {
-    constructor(element) {
-        this.$container = $(element);
-        this.isReadonly = false;
-        this.findDOMElement();
-        this.initPlugin();
-    }
+  constructor(element) {
+    this.$container = $(element);
+    this.findDOMElement();
+    this.initPlugin();
+  }
 
-    findDOMElement() {
-        this.rate_button = this.$container.find('.js-widget-rate-button');
-        this.valueRating = this.rate_button.attr('data-rating');
-        this.valueReadonly = this.rate_button.attr('data-readonly');
+  findDOMElement() {
+    this.rate_button = this.$container.find('.js-rate-button__container')
+    this.valueRating = this.rate_button.attr('data-rating')
+    this.valueReadonly = this.rate_button.attr('data-readonly')
+  }
+  initPlugin() {
+    if(this.valueReadonly === 'true') {
+      this.valueReadonly=true
+    } else {
+      this.valueReadonly=false
     }
-    initPlugin() {
-        if(this.valueReadonly == 'true') this.valueReadonly=true
-        else this.valueReadonly=false
-
-        this.rate_button.barrating({
-            theme: 'css-stars',
-            showSelectedRating: false,
-            initialRating: this.valueRating,
-            readonly: this.valueReadonly
-        });
-    }
+    
+    this.rate_button.barrating({
+      theme: 'css-stars',
+      showSelectedRating: false,
+      initialRating: this.valueRating,
+      readonly: this.valueReadonly
+    });
+  }
 }
 
 export default RateButton
