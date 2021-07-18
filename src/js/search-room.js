3e4fcc274a9b18f5cd7a66c0bfa3d4caa40d941b
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-07-18 22:06:38 +0300

    Добавление страницы search-room

--- src/js/search-room.js
+++ src/js/search-room.js
@@ -1,1 +1,8 @@
 import '../scss/search-room.scss';
+
+import '../templates/filter-date-dropdown/filter-date-dropdown-init';
+import '../templates/dropdown/dropdown-init';
+import '../templates/range-slider/range-slider-init';
+import '../templates/checkbox-list/checkbox-list-init';
+import '../templates/cutaway-room-card/cutaway-room-card-init';
+import '../templates/pagination/pagination-init';
