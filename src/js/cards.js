bad75b88b761434bd951219e91612f1b0222c802
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-07-18 21:58:26 +0300

    Добавление страницы cards

---
+++ src/js/cards.js
@@ -0,0 +1,7 @@
+import '../scss/cards.scss'
+
+import '../templates/start-filter/start-filter-init'
+import '../templates/registration-card/registration-card-init'
+import '../templates/room-info-card/room-info-card-init'
+import '../templates/login-card/login-card-init'
+import '../templates/cutaway-room-card/cutaway-room-card-init'
