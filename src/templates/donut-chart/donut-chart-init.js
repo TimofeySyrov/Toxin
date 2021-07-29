ac15c3843196215f64f1e18f38f4ceb3e3958d08
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-07-30 00:24:26 +0300

    Рефакторинг donut-chart

--- src/templates/donut-chart/donut-chart-init.js
+++ src/templates/donut-chart/donut-chart-init.js
@@ -2,6 +2,61 @@ import DonutChart from './donut-chart';
 
 $(() => {
   $('.js-donut-chart').each((i, val) => {
-    new DonutChart(val);
+    new DonutChart(val, {
+      options: {
+        textInside: 'голосов'
+      },
+      size: {
+        width: 120,
+        height: 120,
+        margin: 0,
+        radius: {
+          inside: 90
+        }
+      },
+      data: [
+        {id: 'a', value: 130, label: 'Великолепно',
+          colors: {
+            start: {
+              color: '#FFE39C',
+            },
+            end: {
+              color: '#FFBA9C'
+            }
+          }
+        },
+        {id: 'b', value: 65, label: 'Хорошо',
+          colors: {
+            start: {
+              color: '#6FCF97',
+              offset: '70%'
+            },
+            end: {
+              color: '#66D2EA'
+            }
+          }
+        },
+        {id: 'c', value: 65, label: 'Удовлетворительно',
+          colors: {
+            start: {
+              color: '#BC9CFF'
+            },
+            end: {
+              color: '#8BA4F9'
+            }
+          }
+        },
+        {id: 'd', value: 0, label: 'Разочарован',
+          colors: {
+            start: {
+              color: '#919191'
+            },
+            end: {
+              color: '#3D4975'
+            }
+          }
+        }
+      ]
+    });
   });
 });
--- src/templates/donut-chart/donut-chart.js
+++ src/templates/donut-chart/donut-chart.js
@@ -1,132 +1,138 @@
 import * as d3 from "d3";
 
-class DonutChart{
-  constructor(elem){
-    this.elem = elem;
+class DonutChart {
+  constructor(elem, options) {
+    this.$el = elem
+    this.$chartBox
+    this.options = options
 
-    this.init();
+    this.#render()
   }
 
-  init(){
-    var width = 120,
-    height = 120,
-    margin = 0;
+  #render() {
+    const {size, data, options} = this.options
 
-    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-    var radius = Math.min(width, height) / 2 - margin;
+    this.$el.innerHTML += `<div class="donut-chart__box-diagram"></div>`
+    this.$chartBox = this.$el.querySelector('.donut-chart__box-diagram')
 
-    // append the svg object to the div called 'my_dataviz'
-    var svg = d3.select(this.elem)
-      .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-      .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-    
-    var defs = svg.append("defs");
+    this.#initChart(size, data)
+    this.#createChartTextInside(data, options)
+    this.#createChartLabelsList(data)
+  }
 
-    var gradient_yellow = defs.append("linearGradient")
-      .attr("id", "svgGradientYellow")
-      .attr("x1", "0%")
-      .attr("x2", "100%")
-      .attr("y1", "0%")
-      .attr("y2", "100%");
-    
-    gradient_yellow.append("stop")
-    .attr("class", "start")
-    .attr("offset", "0%")
-    .attr("stop-color", "#FFE39C")
-    .attr("stop-opacity", 1);
-    
-    gradient_yellow.append("stop")
-    .attr("class", "end")
-    .attr("offset", "100%")
-    .attr("stop-color", "#FFBA9C")
-    .attr("stop-opacity", 1);
+  #initChart(size, data) {
+    const chart = this.#createChart(this.$chartBox, size)
+    const chartDataGradients = this.#createChartGradients(chart, data)
 
-    var gradient_green = defs.append("linearGradient")
-      .attr("id", "svgGradientGreen")
-      .attr("x1", "0%")
-      .attr("x2", "100%")
-      .attr("y1", "0%")
-      .attr("y2", "100%");
+    const chartData = {}
+    data.map(item => {
+      chartData[item.id] = Number(item.value)
+    })
 
-    gradient_green.append("stop")
-      .attr("class", "start")
-      .attr("offset", "70%")
-      .attr("stop-color", "#6FCF97")
-      .attr("stop-opacity", 1);
-    
-    gradient_green.append("stop")
-      .attr("class", "end")
-      .attr("offset", "100%")
-      .attr("stop-color", "#66D2EA")
-      .attr("stop-opacity", 1);
+    const chartOutsideRadius = size.radius.outside || Math.min(size.width, size.height) / 2 - size.margin
+    const chartInsideRadius = (Number(size.radius.inside) || 50) * (chartOutsideRadius/100)
 
-    var gradient_purple = defs.append("linearGradient")
-      .attr("id", "svgGradientPurple")
-      .attr("x1", "0%")
-      .attr("x2", "100%")
-      .attr("y1", "0%")
-      .attr("y2", "100%");
+    const color = d3.scaleOrdinal()
+      .domain(chartData)
+      .range(chartDataGradients)
 
-    gradient_purple.append("stop")
-      .attr("class", "start")
-      .attr("offset", "0%")
-      .attr("stop-color", "#BC9CFF")
-      .attr("stop-opacity", 1);
-    
-    gradient_purple.append("stop")
-      .attr("class", "end")
-      .attr("offset", "100%")
-      .attr("stop-color", "#8BA4F9")
-      .attr("stop-opacity", 1);
+    const pie = d3.pie().value(function(d) {return d.value; })
+    const data_ready = pie(d3.entries(chartData))
 
-    var gradient_black = defs.append("linearGradient")
-      .attr("id", "svgGradientBlack")
-      .attr("x1", "0%")
-      .attr("x2", "100%")
-      .attr("y1", "0%")
-      .attr("y2", "100%");
-
-    gradient_black.append("stop")
-      .attr("class", "start")
-      .attr("offset", "0%")
-      .attr("stop-color", "#919191")
-      .attr("stop-opacity", 1);
-    
-    gradient_black.append("stop")
-      .attr("class", "end")
-      .attr("offset", "100%")
-      .attr("stop-color", "#3D4975")
-      .attr("stop-opacity", 1);
-
-    // Create dummy data
-    var data = {a: 130, b: 65, c:65, d: 0}
-
-    // set the color scale
-    var color = d3.scaleOrdinal()
-      .domain(data)
-      .range(["url(#svgGradientYellow)", "url(#svgGradientGreen)", "url(#svgGradientPurple)", "url(#svgGradientBlack)"])
-
-    // Compute the position of each group on the pie:
-    var pie = d3.pie()
-      .value(function(d) {return d.value; })
-    var data_ready = pie(d3.entries(data))
-
-    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-    svg
+    chart
       .selectAll('whatever')
       .data(data_ready)
       .enter()
       .append('path')
       .attr('d', d3.arc()
-        .innerRadius(54)        // This is the size of the donut hole
-        .outerRadius(radius)
-      )
+        .innerRadius(chartInsideRadius)
+        .outerRadius(chartOutsideRadius))
       .attr('fill', function(d){ return(color(d.data.key)) })
       .attr("stroke", "white")
       .style("stroke-width", "2px")
+  }
+
+  #createChart(chart, size) {
+    const svg = d3.select(chart)
+      .append("svg")
+        .attr("class", "donut-chart__diagram")
+        .attr("width", size.width)
+        .attr("height", size.height)
+      .append("g")
+        .attr("transform", "translate(" + size.width / 2 + "," + size.height / 2 + ")");
+
+    return svg
+  }
+
+  #createChartGradients(chart, data){
+    const defs = chart.append("defs");
+    let dataGradients = []
+
+    data.map(item => {
+      const gradient = defs.append("linearGradient")
+        .attr("id", item.id + 'gradientColor' || "svgGradient")
+        .attr("x1", "0%")
+        .attr("x2", "100%")
+        .attr("y1", "0%")
+        .attr("y2", "100%");
+
+      // Start gradient color
+      gradient.append("stop")
+        .attr("class", "start")
+        .attr("offset", item.colors.start.offset || "0%")
+        .attr("stop-color", item.colors.start.color || "#fff")
+        .attr("stop-opacity", item.colors.start.opacity || 1);
+
+      // End gradient color
+      gradient.append("stop")
+        .attr("class", "end")
+        .attr("offset", item.colors.end.offset || "100%")
+        .attr("stop-color", item.colors.end.color || "#000")
+        .attr("stop-opacity", item.colors.end.opacity || 1);
+
+      return dataGradients.push(`url(#${item.id}gradientColor)`)
+    })
+
+    return dataGradients
+  }
+
+  #createChartTextInside(data, options){
+    let total = 0;
+    data.map(item => {
+      return total += Number(item.value)
+    })
+    const templateTextInside = `
+      <div class="donut-chart__text-inside">
+        <span class="donut-chart__number">${total}</span>
+        <span class="donut-chart__label">${options.textInside}</span>
+      </div>`
+
+    this.$chartBox.innerHTML += templateTextInside
+  }
+
+  #createChartLabelsList(data){
+    const items = data.map(item => {
+      return `
+      <li class="donut-chart__list-item">
+        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="5" cy="5" r="5" fill="url(${'#'+item.id+'gradientColor'})"/>
+          <defs>
+            <linearGradient id="${item.id+'gradientColor'}" x1="5" y1="0" x2="5" y2="10" gradientUnits="userSpaceOnUse">
+              <stop stop-color="${item.colors.start.color}"/>
+              <stop offset="1" stop-color="${item.colors.end.color}"/>
+            </linearGradient>
+          </defs>
+        </svg>
+        <span class="donut-chart__list-text">${item.label}</span>
+      </li>
+      `
+    })
+  
+    this.$el.innerHTML += `
+      <ul class="donut-chart__list">
+        ${items.join('')}
+      </ul
+    `
   }
 }
 
