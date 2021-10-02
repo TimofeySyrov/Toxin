50a1fc524bb6520f2bf041eab407060b1f8b265d
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-10-02 14:14:10 +0300

    Рефакторинг под eslint airbnb

--- src/templates/text-field/text-field-init.js
+++ src/templates/text-field/text-field-init.js
@@ -1,12 +1,15 @@
 import TextField from './text-field';
 
 $(() => {
-  let $textFields = $('.js-text-field');
+  const $textFields = $('.js-text-field');
+
   $textFields.each((i, val) => {
-    let textInput = $(val).find('.js-text-field-input');
-    let isMaskedTextField = $(textInput).hasClass('text-field__masked');
-    if(isMaskedTextField) {
-      new TextField(val);
+    const textInput = $(val).find('.js-text-field-input');
+    const isMaskedTextField = $(textInput).hasClass('text-field__masked');
+
+    if (isMaskedTextField) {
+      const textField = new TextField(val);
+      textField();
     }
   });
 });
--- src/templates/text-field/text-field.js
+++ src/templates/text-field/text-field.js
@@ -1,28 +1,30 @@
 import 'inputmask/dist/jquery.inputmask.min';
 
 class TextField {
-    constructor(container) {
-        this.$textField = $(container).find('.js-text-field-input');
-        this.setMasks();
-      }
-    
-      setMasks() {
-        const isMaskedTextField = this.$textField.hasClass('text-field__masked');
-        if (isMaskedTextField) {
-          this.$textField.inputmask({
-            alias: 'datetime',
-            inputFormat: 'dd.mm.yyyy',
-            placeholder: 'ДД.ММ.ГГГГ',
-          });
-        }
-      }
-    
-      getElement() {
-        return this.$textField;
-      }
-      eventListenerBind(type, fn) {
-        if(this.$textField) this.$textField.on(type, fn);
-      }
-    
+  constructor(container) {
+    this.$textField = $(container).find('.js-text-field-input');
+    this.setMasks();
+  }
+
+  setMasks() {
+    const isMaskedTextField = this.$textField.hasClass('text-field__masked');
+    if (isMaskedTextField) {
+      this.$textField.inputmask({
+        alias: 'datetime',
+        inputFormat: 'dd.mm.yyyy',
+        placeholder: 'ДД.ММ.ГГГГ',
+      });
+    }
+  }
+
+  getElement() {
+    return this.$textField;
+  }
+
+  eventListenerBind(type, fn) {
+    if (this.$textField) {
+      this.$textField.on(type, fn);
+    }
+  }
 }
 export default TextField;
