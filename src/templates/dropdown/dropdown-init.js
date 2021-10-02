ab5bac0416c610af27564b7e0046a03e8a2646b1
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-10-02 17:16:24 +0300

    Рефакторинг dropdown под eslint airbnb

--- src/templates/dropdown/dropdown-init.js
+++ src/templates/dropdown/dropdown-init.js
@@ -1,7 +1,6 @@
 import Dropdown from './dropdown';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const dropdown_items = document.querySelectorAll('.js-dropdown');
-
-  dropdown_items.forEach((item) => new Dropdown(item));
+  const dropdownDOMs = document.querySelectorAll('.js-dropdown');
+  dropdownDOMs.forEach((item) => new Dropdown(item));
 });
--- src/templates/dropdown/dropdown.js
+++ src/templates/dropdown/dropdown.js
@@ -1,55 +1,48 @@
 class Dropdown {
   constructor(container) {
     this.container = container;
-    
+
     this.findDomElement();
     this.init();
   }
 
-  findDomElement(){
+  findDomElement() {
     this.dropType = this.container.getAttribute('dropType');
     this.dropIsOpen = this.container.getAttribute('isopen');
-
     this.dropName = this.container.querySelector('.js-dropdown-name');
     this.dropTittle = this.container.querySelector('.js-dropdown-name-text');
     this.dropIcon = this.container.querySelector('.js-dropdown-icon');
-    
-
     this.dropList = this.container.querySelector('.js-dropdown-list');
     this.dropCounts = this.container.querySelectorAll('.js-dropdown-number');
     this.dropItems = this.dropList.querySelectorAll('.js-dropdown-item');
 
-    
-    if(this.dropType == 'guests'){
+    if (this.dropType === 'guests') {
       this.dropConfirm = this.dropList.querySelector('.js-dropdown-confirm');
       this.dropClear = this.dropList.querySelector('.js-dropdown-clear');
     }
-    
   }
+
   init() {
-    this.check_dropIsOpen();
-    this.check_minus_plus_buttons();
-
-    this.eventListener_dropList();
+    this.checkDropIsOpen();
+    this.checkMinusPlusButtons();
+    this.eventListenerDropList();
     this.eventListenerList();
+    this.checkCounts();
 
-    this.check_counts();
-
-    if(this.dropType == 'guests'){
-      this.eventListener_confirm_button();
-      this.eventListener_clear_button();
-    }
-    
-  }
-
-  check_dropIsOpen(){
-    if(this.dropIsOpen == 'true'){
-      this.open_dropList();
+    if (this.dropType === 'guests') {
+      this.eventListenerConfirmButton();
+      this.eventListenerClearButton();
     }
   }
 
-  open_dropList(){
-    if(!(this.dropList.classList.contains('dropdown__list-active'))){
+  checkDropIsOpen() {
+    if (this.dropIsOpen === 'true') {
+      this.openDropList();
+    }
+  }
+
+  openDropList() {
+    if (!(this.dropList.classList.contains('dropdown__list-active'))) {
       this.dropList.classList.add('dropdown__list-active');
       this.dropName.classList.add('dropdown__name-active');
       this.dropIcon.classList.add('dropdown__icon-active');
@@ -60,216 +53,214 @@ class Dropdown {
     }
   }
 
-  eventListener_dropList(){
-    this.dropName.addEventListener('click', this.open_dropList.bind(this));
+  eventListenerDropList() {
+    this.dropName.addEventListener('click', this.openDropList.bind(this));
   }
 
-  check_counts(){
-    var num = [];
-    for(const item of this.dropCounts){
+  checkCounts() {
+    const num = [];
+
+    for (const item of this.dropCounts) {
       num.push(Number(item.innerHTML));
     }
-    this.display_count(num);
+    this.displayCount(num);
   }
 
-  display_count(x){
-    if(this.dropType == 'room-about'){
-      let [room, bedroom, bathroom] = x,
-      room_txt, bed_txt, bath_txt = '';
+  displayCount(x) {
+    if (this.dropType === 'room-about') {
+      const [room, bedroom, bathroom] = x;
+      let roomDesc,
+        bedDesc,
+        bathDesc = '';
 
-    if(room > 0) {
-      if(room == 1) {
-        room_txt = `${room} спальня`;
-      } else if(room > 1 && room < 5) {
-        room_txt = `${room} спальни`;
-      } else if(room > 4) {
-        room_txt = `${room} спален`;
+      if (room > 0) {
+        if (room === 1) {
+          roomDesc = `${room} спальня`;
+        } else if (room > 1 && room < 5) {
+          roomDesc = `${room} спальни`;
+        } else if (room > 4) {
+          roomDesc = `${room} спален`;
+        }
+        this.dropTittle.innerHTML = `${roomDesc}...`;
+      } else {
+        roomDesc = '';
       }
-      this.dropTittle.innerHTML = `${room_txt}...`;        
-    } else {
-      room_txt = '';
-    }
-    
-    if(bedroom > 0) {
-      if(bedroom == 1) {
-        bed_txt = `${bedroom} кровать`;
-      } else if(bedroom > 1 && bedroom < 5) {
-        bed_txt = `${bedroom} кровати`;
-      } else if(bedroom > 4) {
-        bed_txt = `${bedroom} кроватей`;
+
+      if (bedroom > 0) {
+        if (bedroom === 1) {
+          bedDesc = `${bedroom} кровать`;
+        } else if (bedroom > 1 && bedroom < 5) {
+          bedDesc = `${bedroom} кровати`;
+        } else if (bedroom > 4) {
+          bedDesc = `${bedroom} кроватей`;
+        }
+        this.dropTittle.innerHTML = `${bedDesc}...`;
+      } else {
+        bedDesc = '';
       }
-      this.dropTittle.innerHTML = `${bed_txt}...`;
-    } else {
-      bed_txt = '';
-    }
-    
-    
-    if(bathroom > 0) {
-      if(bathroom == 1) {
-        bath_txt = `${bathroom} ванная комн`;
-      } else if(bathroom > 1 && bathroom < 5) {
-        bath_txt = `${bathroom} ванные комн`;
-      } else if(bathroom > 4) {
-        bath_txt = `${bathroom} ванных комн`;
+
+      if (bathroom > 0) {
+        if (bathroom === 1) {
+          bathDesc = `${bathroom} ванная комн`;
+        } else if (bathroom > 1 && bathroom < 5) {
+          bathDesc = `${bathroom} ванные комн`;
+        } else if (bathroom > 4) {
+          bathDesc = `${bathroom} ванных комн`;
+        }
+        this.dropTittle.innerHTML = `${bathDesc}...`;
+      } else {
+        bathDesc = '';
       }
-      this.dropTittle.innerHTML = `${bath_txt}...`;
-    } else {
-      bath_txt = '';
+
+      if (room > 0 && bedroom > 0) {
+        this.dropTittle.innerHTML = `${roomDesc}, ${bedDesc}...`;
+      } else if (room > 0 && bathroom > 0) {
+        this.dropTittle.innerHTML = `${roomDesc}, ${bathDesc}...`;
+      } else if (bedroom > 0 && bathroom > 0) {
+        this.dropTittle.innerHTML = `${bedDesc}, ${bathDesc}...`;
+      }
+
+      if (room > 0 && bedroom > 0 && bathroom > 0) {
+        this.dropTittle.innerHTML = `${roomDesc}, ${bedDesc}, ${bathDesc}...`;
+      }
+
+      if (room === 0 && bedroom === 0 && bathroom === 0) {
+        this.dropTittle.innerHTML = 'спальни, кровати, ванные комн...';
+      }
+
+      localStorage.setItem('NumRoomAbout', JSON.stringify([room, bedroom, bathroom]));
     }
 
-    if(room > 0 && bedroom > 0){
-      this.dropTittle.innerHTML = `${room_txt}, ${bed_txt}...`;
-    } else if(room > 0 && bathroom > 0) {
-      this.dropTittle.innerHTML = `${room_txt}, ${bath_txt}...`;
-    } else if(bedroom > 0 && bathroom > 0){
-      this.dropTittle.innerHTML = `${bed_txt}, ${bath_txt}...`;
-    }
+    if (this.dropType === 'guests') {
+      let adults = '';
+      let babies = '';
+      const adultsCount = x[0] + x[1];
+      const babiesCount = x[2];
 
-    if(room > 0 && bedroom > 0 && bathroom > 0){
-      this.dropTittle.innerHTML = `${room_txt}, ${bed_txt}, ${bath_txt}...`;
-    }
-
-    if(room == 0 && bedroom == 0 && bathroom == 0) {
-      this.dropTittle.innerHTML = `спальни, кровати, ванные комн...`;
-    }
-
-    localStorage.setItem('NumRoomAbout', JSON.stringify([room, bedroom, bathroom]));
-
-  }
-    if(this.dropType == 'guests'){
-      var string_adults = '';
-      var string_babies = '';
-
-      var adults_count = x[0]+x[1];
-      var babies_count = x[2];
-
-      if(adults_count > 0) {
-        if(adults_count == 1) {
-          string_adults = `${adults_count} гость`;
-        } else if(adults_count > 1 && adults_count < 5) {
-          string_adults = `${adults_count} гостя`;
-        } else if(adults_count > 4) {
-          string_adults = `${adults_count} гостей`;
+      if (adultsCount > 0) {
+        if (adultsCount === 1) {
+          adults = `${adultsCount} гость`;
+        } else if (adultsCount > 1 && adultsCount < 5) {
+          adults = `${adultsCount} гостя`;
+        } else if (adultsCount > 4) {
+          adults = `${adultsCount} гостей`;
         }
 
-        this.dropTittle.innerHTML = string_adults;
-        this.display_clear_button();
+        this.dropTittle.innerHTML = adults;
+        this.displayClearButton();
       } else {
         this.dropTittle.innerHTML = 'Сколько гостей';
-        this.delete_clear_button();
+        this.deleteClearButton();
       }
 
-      if(babies_count > 0) {
-        if(babies_count == 1) {
-          string_babies = `${babies_count} младенец`;
-        } else if(babies_count > 1 && babies_count < 5) {
-          string_babies = `${babies_count} младенца`;
-        } else if(babies_count > 4) {
-          string_babies = `${babies_count} младенцев`;
+      if (babiesCount > 0) {
+        if (babiesCount === 1) {
+          babies = `${babiesCount} младенец`;
+        } else if (babiesCount > 1 && babiesCount < 5) {
+          babies = `${babiesCount} младенца`;
+        } else if (babiesCount > 4) {
+          babies = `${babiesCount} младенцев`;
         } else {
-          string_babies = '';
+          babies = '';
         }
       }
-      if(adults_count > 0 && babies_count > 0) {
-        this.dropTittle.innerHTML = `${string_adults}, ${string_babies}`;
+      if (adultsCount > 0 && babiesCount > 0) {
+        this.dropTittle.innerHTML = `${adults}, ${babies}`;
       }
 
-      localStorage.setItem('NumGuests', JSON.stringify([adults_count, babies_count]));
+      localStorage.setItem('NumGuests', JSON.stringify([adultsCount, babiesCount]));
     }
   }
 
-  eventListenerList(){
+  eventListenerList() {
     this.dropList.addEventListener('click', this.handlerList.bind(this));
   }
 
-  handlerList(event){
-    let target = event.target;
+  handlerList(event) {
+    const target = event.target;
 
-    if(target.classList.contains('js-dropdown-plus')) {
-      this.plus_button(target);
-    } else if(target.classList.contains('js-dropdown-minus')) {
-      this.minus_button(target);
+    if (target.classList.contains('js-dropdown-plus')) {
+      this.plusButton(target);
+    } else if (target.classList.contains('js-dropdown-minus')) {
+      this.minusButton(target);
     }
   }
 
-  plus_button(elem){
-    var number = elem.previousElementSibling;
-    number.innerHTML = Number(number.innerHTML)+1;
-    this.check_minus_plus_buttons();
-    if(this.dropType == 'room-about'){
-      this.check_counts();
+  plusButton(elem) {
+    const number = elem.previousElementSibling;
+    number.innerHTML = Number(number.innerHTML) + 1;
+    this.checkMinusPlusButtons();
+    if (this.dropType === 'room-about') {
+      this.checkCounts();
     }
-    
   }
 
-  minus_button(elem){
-    if(Number(elem.nextElementSibling.innerHTML) > 0){
-      elem.nextElementSibling.innerHTML = Number(elem.nextElementSibling.innerHTML)-1;
+  minusButton(elem) {
+    if (Number(elem.nextElementSibling.innerHTML) > 0) {
+      elem.nextElementSibling.innerHTML = `${Number(elem.nextElementSibling.innerHTML) - 1}`;
     }
-    this.check_minus_plus_buttons();
-    if(this.dropType == 'room-about'){
-      this.check_counts();
+    this.checkMinusPlusButtons();
+    if (this.dropType === 'room-about') {
+      this.checkCounts();
     }
-    
   }
 
-  check_minus_plus_buttons(){
-    for(const btn of this.dropItems){
+  checkMinusPlusButtons() {
+    for (const btn of this.dropItems) {
       btn.number = btn.querySelector('.js-dropdown-number');
       btn.plus = btn.querySelector('.js-dropdown-plus');
       btn.minus = btn.querySelector('.js-dropdown-minus');
 
-      if(!(btn.plus.classList.contains('dropdown__list-item-count-btn-active'))){
+      if (!(btn.plus.classList.contains('dropdown__list-item-count-btn-active'))) {
         btn.plus.classList.add('dropdown__list-item-count-btn-active');
       }
 
-      if(Number(btn.number.innerHTML) <= 0) {
+      if (Number(btn.number.innerHTML) <= 0) {
         btn.number.innerHTML = 0;
-        if(btn.minus.classList.contains('dropdown__list-item-count-btn-active')){
+        if (btn.minus.classList.contains('dropdown__list-item-count-btn-active')) {
           btn.minus.classList.remove('dropdown__list-item-count-btn-active');
         }
       }
 
-      if(Number(btn.number.innerHTML) > 0) {
-        if(!(btn.minus.classList.contains('dropdown__list-item-count-btn-active'))){
+      if (Number(btn.number.innerHTML) > 0) {
+        if (!(btn.minus.classList.contains('dropdown__list-item-count-btn-active'))) {
           btn.minus.classList.add('dropdown__list-item-count-btn-active');
         }
       }
     }
   }
 
-  eventListener_confirm_button(){
-    this.dropConfirm.addEventListener('click', this.confirm_button.bind(this));
+  eventListenerConfirmButton() {
+    this.dropConfirm.addEventListener('click', this.confirmButton.bind(this));
   }
 
-  confirm_button(){
-    this.check_counts();
+  confirmButton() {
+    this.checkCounts();
   }
 
-  eventListener_clear_button(){
-    this.dropClear.addEventListener('click', this.clear_button.bind(this));
+  eventListenerClearButton() {
+    this.dropClear.addEventListener('click', this.clearButton.bind(this));
   }
 
-  clear_button(){
-    for(const count of this.dropCounts){
+  clearButton() {
+    for (const count of this.dropCounts) {
       count.innerHTML = 0;
     }
-    this.check_counts();
-    this.check_minus_plus_buttons();
+    this.checkCounts();
+    this.checkMinusPlusButtons();
   }
 
-  delete_clear_button(){
-    if(this.dropClear.classList.contains('dropdown__btns-clear-active')){
-      this.dropClear.classList.remove('dropdown__btns-clear-active')
+  deleteClearButton() {
+    if (this.dropClear.classList.contains('dropdown__btns-clear-active')) {
+      this.dropClear.classList.remove('dropdown__btns-clear-active');
     }
   }
 
-  display_clear_button(){
-    if(!(this.dropClear.classList.contains('dropdown__btns-clear-active'))){
-      this.dropClear.classList.add('dropdown__btns-clear-active')
+  displayClearButton() {
+    if (!(this.dropClear.classList.contains('dropdown__btns-clear-active'))) {
+      this.dropClear.classList.add('dropdown__btns-clear-active');
     }
   }
-
 }
 
-export default Dropdown
+export default Dropdown;
