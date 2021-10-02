7e78764a0145211509c2c4fdf54a7e5cc76cb44d
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-10-02 15:29:09 +0300

    Рефакторинг registration-card под eslint airbnb

--- src/templates/registration-card/registration-card-init.js
+++ src/templates/registration-card/registration-card-init.js
@@ -1,6 +1,6 @@
 import RegCard from './registration-card';
 
 $(() => {
-  let $regCardBody = $('.js-registration-card');
+  const $regCardBody = $('.js-registration-card');
   new RegCard($regCardBody);
 });
--- src/templates/registration-card/registration-card.js
+++ src/templates/registration-card/registration-card.js
@@ -2,62 +2,62 @@ import '../text-field/text-field-init';
 
 class RegCard {
   constructor(elem) {
-    this.container = elem;
+    this.$container = $(elem);
 
     this.init();
-    this.register_btn_eventListener();
+    this.bindEventListener();
   }
 
-  init(){
-    this.firstNameСontainer = $(this.container).find('.js-registration-card-first-name');
-    this.lastNameСontainer = $(this.container).find('.js-registration-card-last-name');
-
-    this.emailСontainer = $(this.container).find('.js-registration-card-email');
-    this.passwordСontainer = $(this.container).find('.js-registration-card-password');
-
-    this.registerButton = $(this.container).find('.js-registration-card-registr');
+  init() {
+    this.$firstNameСontainer = this.$container.find('.js-registration-card-first-name');
+    this.$lastNameСontainer = this.$container.find('.js-registration-card-last-name');
+    this.$emailСontainer = this.$container.find('.js-registration-card-email');
+    this.$passwordСontainer = this.$container.find('.js-registration-card-password');
+    this.$registerButton = this.$container.find('.js-registration-card-registr');
 
     this.listToSafe = [
-      this.firstNameСontainer,
-      this.lastNameСontainer,
-      this.emailСontainer,
-      this.passwordСontainer
+      this.$firstNameСontainer,
+      this.$$lastNameСontainer,
+      this.$emailСontainer,
+      this.$passwordСontainer,
     ];
   }
 
-  getElementsChanges(){
-    var uFirstName, uLastName, uEmail, uPassword;
+  getElementsChanges() {
+    let uFirstName,
+      uLastName,
+      uEmail,
+      uPassword;
 
-    for(const item of this.listToSafe){
-      
-      var itemBody = item.find('input.text-field__input');
-      var itemValue = $(itemBody).val();
-      
-      if(itemValue){
-        if(item.hasClass('js-registration-card-first-name')){
-          uFirstName = itemValue;
-        } else if (item.hasClass('js-registration-card-last-name')){
-          uLastName = itemValue;
-        } else if (item.hasClass('js-registration-card-email')){
-          uEmail = itemValue;
-        } else if (item.hasClass('js-registration-card-password')){
-          uPassword = itemValue;
+    for (const item of this.listToSafe) {
+      const $itemBody = item.find('input.text-field__input');
+      const $itemValue = $itemBody.val();
+
+      if ($itemValue) {
+        if (item.hasClass('js-registration-card-first-name')) {
+          uFirstName = $itemValue;
+        } else if (item.hasClass('js-registration-card-last-name')) {
+          uLastName = $itemValue;
+        } else if (item.hasClass('js-registration-card-email')) {
+          uEmail = $itemValue;
+        } else if (item.hasClass('js-registration-card-password')) {
+          uPassword = $itemValue;
         }
       }
     }
 
-    if(uFirstName && uLastName && uEmail && uPassword){
-      var arr = [uEmail, uPassword, uFirstName, uLastName];
+    if (uFirstName && uLastName && uEmail && uPassword) {
+      const arr = [uEmail, uPassword, uFirstName, uLastName];
+
       localStorage.setItem(uEmail, JSON.stringify(arr));
     } else {
       alert('Вы пропустили одну из форм! Заполните её');
     }
   }
 
-  register_btn_eventListener(){
-    this.registerButton.on('click', this.getElementsChanges.bind(this));
+  bindEventListener() {
+    this.$registerButton.on('click', this.getElementsChanges.bind(this));
   }
-  
 }
 
 export default RegCard;
