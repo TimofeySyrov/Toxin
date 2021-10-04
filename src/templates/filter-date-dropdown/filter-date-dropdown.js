bc7093495186bf7cf596d1b65bc7f2e2b786c2a3
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-10-04 09:27:41 +0300

    Рефакторинг filter-date-dropdown

--- src/templates/filter-date-dropdown/filter-date-dropdown.js
+++ src/templates/filter-date-dropdown/filter-date-dropdown.js
@@ -3,10 +3,14 @@ import Calendar from '../calendar/calendar';
 class FilterDateDropdown {
   constructor(container) {
     this.container = container;
-    this.dropInput = this.container.querySelector('.js-filter-date-dropdown__input');
-    this.isOpen = this.container.getAttribute('data-calendar-isopen');
+    this.calendarBody = this.container.querySelector('.js-filter-date-dropdown__calendar');
+    this.calendarInput = this.container.querySelector('.js-filter-date-dropdown__input');
+    this.isCalendarOpen = Boolean.prototype.valueOf(this.container.getAttribute('data-calendar-isopen'));
 
-    new Calendar(this.container, this.isOpen, this.dropInput);
+    new Calendar(this.calendarBody, {
+      isOpen: this.isCalendarOpen,
+      inputs: [this.calendarInput],
+    });
   }
 }
 
