43afde767513ce3bf70b61ca22923f0c0ac71802
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-10-21 21:52:01 +0300

    Рефакторинг отображения дат

--- src/templates/date-dropdown/date-dropdown.js
+++ src/templates/date-dropdown/date-dropdown.js
@@ -17,7 +17,7 @@ class DateDropdown {
       body: this.calendarBody,
       isOpen: true,
       options: {
-        onSelect: (formattedDate, date) => this.setDates(date)
+        onSelect: (formattedDate) => this.setDates(formattedDate)
       }
     });
   }
@@ -30,11 +30,35 @@ class DateDropdown {
   }
 
   setDates(dates) {
-    const [ arrDate, depDate ] = dates;
     const { arrivalInput, depatureInput } = this;
+    const inputPlaceholder = 'ДД.ММ.ГГГГ'.toUpperCase();
 
-    arrivalInput.innerHTML = arrDate;
-    depatureInput.innerHTML = depDate;
+    const isSelectedDate = (date) => {
+      const isUndefined  = date === undefined;
+      const isEmpty = date === '';
+
+      if (!isUndefined && !isEmpty) {
+        return true;
+      }
+
+      return false;
+    };
+
+    if (isSelectedDate(dates)) {
+      const isRange = dates.indexOf('-') !== -1;
+
+      if (isRange) {
+        const [arrDate, depDate] = dates.split('-');
+
+        arrivalInput.innerHTML = arrDate;
+        depatureInput.innerHTML = depDate;
+      } else {
+        arrivalInput.innerHTML = dates;
+      }
+    } else {
+      arrivalInput.innerHTML = inputPlaceholder;
+      depatureInput.innerHTML = inputPlaceholder;
+    }
   }
 }
 export default DateDropdown;
