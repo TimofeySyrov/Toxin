be6e0845aafd945b725e2e11e18b964849274a8d
Timofey Syrov <warfacetopic@mail.ru>
2021-10-21 21:26:13 +0500

    Рефакторинг date-dropdown, добавление логики работы с Calendar

--- src/templates/date-dropdown/date-dropdown.js
+++ src/templates/date-dropdown/date-dropdown.js
@@ -1,21 +1,40 @@
 import Calendar from '../calendar/calendar';
 
 class DateDropdown {
-  constructor(container) {
-    this.container = container;
-    this.calendarBody = this.container.querySelector('.js-date-dropdown__calendar-container');
-    this.arrivalInput = this.container.querySelector('.js-date-dropdown-arrival');
-    this.depatureInput = this.container.querySelector('.js-date-dropdown-depature');
-    this.isCalendarOpen = Boolean.prototype.valueOf(this.container.getAttribute('data-calendar-isOpen'));
+  constructor(body) {
+    this.body = body;
+    this.calendarBody = this.body.querySelector('.js-date-dropdown__calendar-container');
+    this.arrivalInput = this.body.querySelector('.js-date-dropdown-arrival');
+    this.depatureInput = this.body.querySelector('.js-date-dropdown-depature');
+    this.isCalendarOpen = this.body.getAttribute('data-calendar-isOpen') === 'true';
 
     this.init();
+    this.bindEventListenerInputs();
   }
 
   init() {
-    new Calendar(this.calendarBody, {
-      isOpen: this.isCalendarOpen,
-      inputs: [this.arrivalInput, this.depatureInput],
+    this.calendar = new Calendar({
+      body: this.calendarBody,
+      isOpen: true,
+      options: {
+        onSelect: (formattedDate, date) => this.setDates(date)
+      }
     });
+  }
+
+  bindEventListenerInputs() {
+    const { calendar } = this;
+
+    this.arrivalInput.addEventListener('click', calendar.checkIsOpen.bind(calendar));
+    this.depatureInput.addEventListener('click', calendar.checkIsOpen.bind(calendar));
+  }
+
+  setDates(dates) {
+    const [ arrDate, depDate ] = dates;
+    const { arrivalInput, depatureInput } = this;
+
+    arrivalInput.innerHTML = arrDate;
+    depatureInput.innerHTML = depDate;
   }
 }
 export default DateDropdown;
