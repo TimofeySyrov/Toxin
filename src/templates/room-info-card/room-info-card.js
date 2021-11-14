c8163b161be8cb6ca681a5cc928bc64cd4555cca
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-11-14 05:56:54 +0300

    добавлена логика расчета стоимости номера

--- src/templates/room-info-card/room-info-card.js
+++ src/templates/room-info-card/room-info-card.js
@@ -1,9 +1,63 @@
-import '../date-dropdown/date-dropdown-init';
+import DateDropdown from '../date-dropdown/date-dropdown';
 import '../dropdown/dropdown-init';
 
 class RoomMainCard {
-  constructor(elem) {
-    this.$container = $(elem);
+  constructor(body) {
+    this.body = body;
+    this.pricePerDay = this.body.querySelector('.js-room-info-card__price-per-day');
+    this.daysPeriod = this.body.querySelector('.js-room-info-card__table-row-period');
+    this.daysPeriodPrice = this.body.querySelector('.js-room-info-card__days-period-price');
+    this.discount = this.body.querySelector('.js-room-info-card__discount');
+    this.additionalCharges = this.body.querySelector('.js-room-info-card__additional-charges');
+    this.finalCost = this.body.querySelector('.js-room-info-card__final-cost');
+
+    this.subscribeToDateChanges();
+  }
+
+  subscribeToDateChanges() {
+    const dateDropdownBody = this.body.querySelector('.js-date-dropdown');
+    this.dateDropdown = new DateDropdown(dateDropdownBody);
+
+    this.dateDropdown.observeDateChanges(this.calculateTheFinalCost.bind(this));
+  }
+
+  getPeriodDays(dates) {
+    const [arrDate, depDate] = dates.split('-').map((date) => {
+      const [day, month, year] = date.split('.').map((item) => parseInt(item));
+
+      return new Date(year, month, day);
+    });
+    const oneDay = 1000 * 60 * 60 * 24; // Get 1 day in milliseconds
+    const periodDays = Math.abs((arrDate.getTime() - depDate.getTime()) / oneDay);
+
+    return periodDays;
+  }
+
+  calculateTheFinalCost(dates) {
+    const isSelectedDate = (date) => {
+      const isUndefined = date === undefined;
+      const isEmpty = date === '';
+
+      if (!isUndefined && !isEmpty) return true;
+      return false;
+    };
+
+    if (isSelectedDate(dates)) {
+      const isRange = dates.indexOf('-') !== -1;
+      
+      if (isRange) {
+        const pricePerDay = this.pricePerDay.innerHTML.replace(/\s/g, '');
+        const periodNumber = this.getPeriodDays(dates);
+        const daysPeriodPrice = parseInt(pricePerDay) * parseInt(periodNumber);
+        const discount = this.discount.innerHTML.replace(/\s/g, '');
+        const additionalCharges = this.additionalCharges.innerHTML.replace(/\s/g, '');
+        const finalCost = (daysPeriodPrice - parseInt(discount)) + parseInt(additionalCharges);
+
+        this.daysPeriod.innerHTML = periodNumber;
+        this.daysPeriodPrice.innerHTML = daysPeriodPrice;
+        this.finalCost.innerHTML = finalCost.toLocaleString();
+      }
+    }
   }
 }
 
