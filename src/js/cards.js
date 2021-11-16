2646858ff550991f75220fa2824216a2afa7a4cc
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-11-16 15:46:42 +0300

    рефакторинг страниц

--- src/js/cards.js
+++ src/js/cards.js
@@ -2,5 +2,4 @@ import '../assets/favicons/favicons';
 import '../scss/cards.scss';
 import '../templates/start-filter/start-filter';
 import '../templates/registration-card/registration-card';
-import '../templates/room-info-card/room-info-card-init';
 import '../templates/cutaway-room-card/cutaway-room-card-init';
