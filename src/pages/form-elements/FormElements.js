271e8be74a1992904e8f65cce6ebf29916b14706
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-12-16 15:55:58 +0300

    Исправление перебора DOM компонентов

--- src/pages/form-elements/FormElements.js
+++ src/pages/form-elements/FormElements.js
@@ -28,23 +28,54 @@ class FormElements {
       checkboxList,
       userComment,
     } = this._getDOMElements();
+    this.textFields = [];
+    this.dropdowns = [];
+    this.dateDropdowns = [];
+    this.filterDateDropdowns = [];
+    this.likeButtons = [];
+    this.rateButtons = [];
+    this.rangeSliders = [];
+    this.paginations = [];
+    this.checkboxLists = [];
+    this.userComments = [];
 
-    this.textFields = textField.map((item) => new TextField(item));
-    this.dropdowns = dropdown.map((item) => new Dropdown(item));
-    this.dateDropdowns = dateDropdown.map((item) => new DateDropdown(item));
-    this.filterDateDropdowns = filterDateDropdown.map((item) => (
-      new FilterDateDropdown(item)
-    ));
-    this.likeButtons = likeButton.map((item) => new LikeButton(item));
-    this.rateButtons = rateButton.map((item) => new RateButton(item));
-    this.rangeSliders = rangeSlider.map((item) => new RangeSlider(item));
-    this.paginations = pagination.map((item) => new Pagination(item));
-    this.CheckboxLists = checkboxList.map((item) => new CheckboxList(item));
-    this.userComments = userComment.map((item) => new UserComment(item));
+    this.paginations.push(new Pagination(pagination, {
+      items: 180,
+      pageSize: 12,
+      pageRange: 3,
+    }));
+    textField.forEach((item) => {
+      this.textFields.push(new TextField(item));
+    });
+    dropdown.forEach((item) => {
+      this.dropdowns.push(new Dropdown(item));
+    });
+    dateDropdown.forEach((item) => {
+      this.dateDropdowns.push(new DateDropdown(item));
+    });
+    filterDateDropdown.forEach((item) => {
+      this.filterDateDropdowns.push(new FilterDateDropdown(item));
+    });
+    likeButton.forEach((item) => {
+      this.likeButtons.push(new LikeButton(item));
+    });
+    rateButton.forEach((item) => {
+      this.rateButtons.push(new RateButton(item));
+    });
+    rangeSlider.forEach((item) => {
+      this.rangeSliders.push(new RangeSlider(item));
+    });
+    checkboxList.forEach((item) => {
+      this.checkboxLists.push(new CheckboxList(item));
+    });
+    userComment.forEach((item) => {
+      this.userComments.push(new UserComment(item));
+    });
   }
 
   _getDOMElements() {
     return {
+      pagination: document.querySelector('.js-pagination-item'),
       textField: document.querySelectorAll('.js-start-filter-item'),
       dropdown: document.querySelectorAll('.js-dropdown-item'),
       dateDropdown: document.querySelectorAll('.js-date-dropdown-item'),
@@ -52,7 +83,6 @@ class FormElements {
       likeButton: document.querySelectorAll('.js-like-button-item'),
       rateButton: document.querySelectorAll('.js-rate-button-item'),
       rangeSlider: document.querySelectorAll('.js-range-slider-item'),
-      pagination: document.querySelectorAll('.js-pagination-item'),
       checkboxList: document.querySelectorAll('.js-checkbox-list-item'),
       userComment: document.querySelectorAll('.js-user-comment-item'),
     };
