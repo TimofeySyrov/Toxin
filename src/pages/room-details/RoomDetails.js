1b17f5d1581ae753f412680829f0ef2fbfe71e1b
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-12-16 13:57:01 +0300

    Инициализация компонентов в классе страницы #1

--- src/pages/room-details/RoomDetails.js
+++ src/pages/room-details/RoomDetails.js
@@ -1,4 +1,37 @@
-import '../../templates/room-info-card/room-info-card-init';
-import '../../templates/donut-chart/donut-chart-init';
-import '../../templates/user-comment/user-comment';
+import RoomInfoCard from '../../templates/room-info-card/room-info-card';
+import DonutChart from '../../templates/donut-chart/donut-chart';
+import UserComment from '../../templates/user-comment/user-comment';
+import dataDonutChartOptions from './utils/dataDonutChartOptions.json';
 import './room-details.scss';
+
+class RoomDetails {
+  constructor() {
+    this._initComponents();
+  }
+
+  _initComponents() {
+    const { donutChart, roomInfoCard, userComment } = this._getDOMElements();
+    const chartOptions = JSON.parse(JSON.stringify(dataDonutChartOptions));
+    this.donutCharts = [];
+    this.roomInfoCards = [];
+    this.userComments = [];
+
+    this.donutCharts.push(new DonutChart(donutChart, chartOptions));
+    roomInfoCard.forEach((item) => {
+      this.roomInfoCards.push(new RoomInfoCard(item));
+    });
+    userComment.forEach((item) => {
+      this.userComments.push(new UserComment(item));
+    });
+  }
+
+  _getDOMElements() {
+    return {
+      donutChart: document.querySelector('.js-donut-chart-item'),
+      roomInfoCard: document.querySelectorAll('.js-room-info-card-item'),
+      userComment: document.querySelectorAll('.js-user-comment-item'),
+    };
+  }
+}
+
+export default new RoomDetails();
