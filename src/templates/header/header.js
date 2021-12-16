77b8903d700b799f1a0658a549e98284273e0543
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-12-16 20:11:27 +0300

    Добавлен бургер для мобильной версии хедера #2

--- src/templates/header/header.js
+++ src/templates/header/header.js
@@ -3,13 +3,28 @@ import HeaderNavMenu from './header-nav-menu/header-nav-menu';
 class Header {
   constructor(domParent) {
     this.body = domParent.querySelector('.js-header');
-    this.headerNavMenuBody = this.body.querySelector('.js-header__nav-menu');
 
+    this._findDOMElements();
     this._init();
+    this._bindEventListeners();
+  }
+
+  _findDOMElements() {
+    this.headerNavMenuBody = this.body.querySelector('.js-header__nav-menu');
+    this.menuButton = this.body.querySelector('.js-header__menu-button');
+    this.menuContainer = this.body.querySelector('.js-header__nav-container');
   }
 
   _init() {
     this.headerNavMenu = new HeaderNavMenu(this.headerNavMenuBody);
+  }
+
+  _bindEventListeners() {
+    this.menuButton.addEventListener('click', this._handleMenuButtonClick.bind(this));
+  }
+
+  _handleMenuButtonClick() {
+    this.menuContainer.classList.toggle('header__nav-container_active');
   }
 }
 
