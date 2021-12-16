cdc0c9cde878968178e97b67b3202caa99977255
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-12-16 13:28:26 +0300

    Инициализация компонентов в классе страницы #1

--- src/pages/cards/Cards.js
+++ src/pages/cards/Cards.js
@@ -1,5 +1,44 @@
-import '../../templates/start-filter/start-filter';
-import '../../templates/registration-card/registration-card';
-import '../../templates/room-info-card/room-info-card-init';
-import '../../templates/cutaway-room-card/cutaway-room-card-init';
+import StartFilter from '../../templates/start-filter/start-filter';
+import RegistrationCard from '../../templates/registration-card/registration-card';
+import RoomInfoCard from '../../templates/room-info-card/room-info-card';
+import CutawayRoomCard from '../../templates/cutaway-room-card/cutaway-room-card';
+import LoginCard from '../../templates/login-card/login-card';
 import './cards.scss';
+
+class Cards {
+  constructor() {
+    this._initComponents();
+  }
+
+  _initComponents() {
+    const {
+      startFilter,
+      registrationCard,
+      roomInfoCard,
+      cutawayRoomCard,
+      loginCard,
+    } = this._getDOMElements();
+
+    this.startFilters = startFilter.map((item) => new StartFilter(item));
+    this.registrationCards = registrationCard.map((item) => (
+      new RegistrationCard(item)
+    ));
+    this.roomInfoCards = roomInfoCard.map((item) => new RoomInfoCard(item));
+    this.cutawayRoomCards = cutawayRoomCard.map((item) => (
+      new CutawayRoomCard(item)
+    ));
+    this.loginCards = loginCard.map((item) => new LoginCard(item));
+  }
+
+  _getDOMElements() {
+    return {
+      startFilter: document.querySelectorAll('.js-start-filter-item'),
+      registrationCard: document.querySelectorAll('.js-registartion-card-item'),
+      roomInfoCard: document.querySelectorAll('.js-room-info-card-item'),
+      cutawayRoomCard: document.querySelectorAll('.js-cutaway-room-card-item'),
+      loginCard: document.querySelectorAll('.js-login-card-item'),
+    };
+  }
+}
+
+export default new Cards();
