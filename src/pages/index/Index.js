a291f3e16b6be120070df96d58a24ade702c6fbf
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-12-16 13:28:11 +0300

    Инициализация компонентов в классе страницы #1

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -1,2 +1,22 @@
-import '../../templates/start-filter/start-filter';
+import StartFilter from '../../templates/start-filter/start-filter';
 import './index.scss';
+
+class Index {
+  constructor() {
+    this._initComponents();
+  }
+
+  _initComponents() {
+    const { startFilter } = this._getDOMElements();
+
+    this.startFilters = startFilter.map((item) => new StartFilter(item));
+  }
+
+  _getDOMElements() {
+    return {
+      startFilter: document.querySelectorAll('.js-start-filter-item'),
+    };
+  }
+}
+
+export default new Index();
