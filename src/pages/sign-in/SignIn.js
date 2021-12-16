2925cf92fedba8fb0efe8e3390fd707a9d28de83
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-12-16 16:08:02 +0300

    Добавлены классы для text-field компонентов #1

--- src/pages/sign-in/SignIn.js
+++ src/pages/sign-in/SignIn.js
@@ -1,1 +1,25 @@
+import LoginCard from '../../templates/login-card/login-card';
 import './sign-in.scss';
+
+class SignIn {
+  constructor() {
+    this._initComponents();
+  }
+
+  _initComponents() {
+    const { loginCard } = this._getDOMElements();
+    this.loginCards = [];
+
+    loginCard.forEach((item) => {
+      this.loginCards.push(new LoginCard(item));
+    });
+  }
+
+  _getDOMElements() {
+    return {
+      loginCard: document.querySelectorAll('.js-login-card-item'),
+    };
+  }
+}
+
+export default new SignIn();
--- src/pages/sign-up/SignUp.js
+++ src/pages/sign-up/SignUp.js
@@ -1,2 +1,25 @@
-import '../../templates/registration-card/registration-card';
+import RegistrationCard from '../../templates/registration-card/registration-card';
 import './sign-up.scss';
+
+class SignUp {
+  constructor() {
+    this._initComponents();
+  }
+
+  _initComponents() {
+    const { registrationCard } = this._getDOMElements();
+    this.registrationCards = [];
+
+    registrationCard.forEach((item) => {
+      this.registrationCards.push(new RegistrationCard(item));
+    });
+  }
+
+  _getDOMElements() {
+    return {
+      registrationCard: document.querySelectorAll('.js-registration-card-item'),
+    };
+  }
+}
+
+export default new SignUp();
