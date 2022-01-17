c9dd45170170b72616b014f16d9d0d080f65b71e
Timofey Syromyatnikov <warfacetopic@mail.ru>
2022-01-17 04:29:44 +0300

    Начальное состояние теперь задается через pug #6

--- src/templates/like-button/like-button.js
+++ src/templates/like-button/like-button.js
@@ -4,13 +4,12 @@ class LikeButton {
 
     this._findDomElements();
     this._bindEventListener();
-    this._setStage();
   }
 
   _findDomElements() {
     this.icon = this.button.querySelector('.js-like-button__icon');
-    this.isChecked = this.button.getAttribute('data-checked');
     this.countLikes = this.button.querySelector('.js-like-button__count');
+    this.isChecked = this.button.hasAttribute('data-checked');
   }
 
   _bindEventListener() {
@@ -18,37 +17,30 @@ class LikeButton {
   }
 
   _handleButtonClick() {
-    this.button.classList.toggle('like-button_active');
+    const { isChecked, countLikes } = this;
 
-    if (this.button.className.includes('like-button_active')) {
+    if (!isChecked) {
       this._setActive();
-      this.countLikes.textContent = Number(this.countLikes.textContent) + 1;
+      countLikes.textContent = Number(countLikes.textContent) + 1;
     } else {
-      this._setUnActive();
+      const isCorrectCount = Number(countLikes.textContent) >= 0;
+      this._removeActive();
 
-      if (this.countLikes.textContent >= 0) {
-        this.countLikes.textContent = Number(this.countLikes.textContent) - 1;
+      if (isCorrectCount) {
+        countLikes.textContent = Number(countLikes.textContent) - 1;
       }
     }
   }
 
-  _setStage() {
-    if (this.isChecked === 'true') {
-      this._setActive();
-    }
-    
-    if (this.isChecked === 'false') {
-      this._setUnActive();
-    }
-  }
-
   _setActive() {
+    this.isChecked = true;
     this.button.classList.add('like-button_active');
     this.icon.classList.add('like-button__icon_active');
     this.icon.innerHTML = 'favorite';
   }
 
-  _setUnActive() {
+  _removeActive() {
+    this.isChecked = false;
     this.button.classList.remove('like-button_active');
     this.icon.classList.remove('like-button__icon_active');
     this.icon.innerHTML = 'favorite_border';
